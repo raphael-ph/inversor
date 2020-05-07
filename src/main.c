6c25ae08dad178a2faf13d4106d3249ca1884e91
Raphael Carvalho <raphasilvaesilva15@gmail.com>
2020-05-07 00:17:44 -0300

    adicionada parte para inverter as palavras

--- src/main.c
+++ src/main.c
@@ -1,14 +1,44 @@
 /* Data de submissao:
- * Nome:
- * RA:
+ * Nome: RAPHAEL CARVALHO DA SILVA E SILVA
+ * RA: 205125
  */
 
 #include <stdio.h>
-const int tam_buffer=100;
+const int tam_buffer = 100;
 
 int main() {
-  char buffer[tam_buffer];
-  fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
-  return 0;
+    char buffer[tam_buffer];
+	char aux_buffer[tam_buffer];
+	int i, j, x;
+	j = 0, i = 0, x = 0;
+    fgets(buffer, tam_buffer, stdin);
+	while(buffer[i] != '\n')
+	{
+		if(buffer[i] == ' ')
+		{
+			x = i;
+			for(i = x-1, x; j < x; i--, j++)
+			{
+				aux_buffer[j] = buffer[i];
+			}
+			aux_buffer[x] = buffer[x];
+			i = x;
+			j = x + 1;
+		}
+		i++;
+	}
+	if(buffer[i] == '\n')
+		{
+			x = i;
+			for(i = x-1, x; j <= x; i--, j++)
+			{
+				aux_buffer[j] = buffer[i];
+			}
+		}
+	aux_buffer[x] = '\n';
+    printf("%s", aux_buffer);
+    return 0;
 }
+
+
+
